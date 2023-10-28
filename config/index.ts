0d485f8c217eb08b1305b40af06e974a89d7af90
chalitguza <32437546+chalitguza@users.noreply.github.com>
2023-10-29 05:34:57 +0700

    Update index.ts

--- config/index.ts
+++ config/index.ts
@@ -3,12 +3,17 @@ export const APP_ID = `${process.env.NEXT_PUBLIC_APP_ID}`
 export const API_KEY = `${process.env.NEXT_PUBLIC_APP_KEY}`
 export const API_URL = `${process.env.NEXT_PUBLIC_API_URL}`
 export const APP_INFO: AppInfo = {
-  title: 'Text Generator APP',
-  description: 'App description',
-  copyright: '',
+  title: 'ผู้ช่วยเขียนโค้ดภาษา PineScriptV.5',
+  description: 'Chatbot ให้คำแนะนำเกี่ยวกับการเขียนโค้ดภาษา PineScript',
+  copyright: 'HAP',
   privacy_policy: '',
-  default_language: 'zh-Hans',
+  default_language: 'th-Hans',
 }
+
+
+export const isShowPrompt = true
+export const promptTemplate = 'I want you to act as a javascript console.'
+
 
 export const API_PREFIX = '/api'
 
